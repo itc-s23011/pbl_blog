3e68ac31898ffb1191352adbd6921cd359c9ccb7
itc-s23011 <s23011@std.it-college.ac.jp>
2024-07-23 12:21:23 +0900

    first commit

---
+++ app/page.js
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+    return (
+        <>
+            <header>HEADER</header>
+            <main>
+                <div>
+                    <h1>
+                        CUBE
+                    </h1>
+                    <p>アウトプットしていくサイト</p>
+                </div>
+            </main>
+            <footer>FOOTER</footer>
+        </>
+    );
+}
